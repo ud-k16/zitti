41d296a503971b027954a75c5d9aeeea4b6ed43c
uma <forjust224@gmail.com>
2023-04-24 04:28:44 +0530

    processing given string for main words added

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import axios from 'axios';
+import { userInput } from './input/userinput.js';
 
-const zitti = async (userInput) => {
-  await axios
-    .post('http://127.0.0.1:3000/addToShoppingList', {
-      itemName: 'SWeet',
-    })
-    .then((result) => {
-      console.log(result.data, 'post result');
-    });
-  axios.get('http://127.0.0.1:3000/getShoppingList').then((result) => {
-    console.log(result.data);
+const zitti = async () => {
+  userInput.forEach((input) => {
+    const inputInSmallCase = input.toLowerCase();
+    //finding out the main word from given input to process further as they have many task related
+    //if match found matchFound is an array of result or null otherwise
+    const matchFound = inputInSmallCase.match(
+      /\bshopping\b|\bnewspaper\b|\bclean\b/
+    );
+    const mainWord = matchFound ? matchFound[0] : '';
+    switch (mainWord) {
+      case 'shopping':
+        console.log('match found for shopping');
+        break;
+      case 'newspaper':
+        break;
+      case 'clean':
+        break;
+      default:
+        console.log('Q:', input, "\nA.Hmm.. I don't know that");
+        break;
+    }
   });
+
+  // await axios
+  //   .post('http://127.0.0.1:3000/addToShoppingList', {
+  //     itemName: 'SWeet',
+  //   })
+  //   .then((result) => {
+  //     console.log(result.data, 'post result');
+  //   });
+  // axios.get('http://127.0.0.1:3000/getShoppingList').then((result) => {
+  //   console.log(result.data);
+  // });
 };
-zitti('Hey. How are you?');
+zitti();
