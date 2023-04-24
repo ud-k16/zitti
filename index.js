c40c90fc2ce9c5f97ff9563f480ed0f0ad2e02b5
uma <forjust224@gmail.com>
2023-04-24 10:07:57 +0530

    temp commit

--- index.js
+++ index.js
@@ -6,17 +6,25 @@ const zitti = async () => {
     const inputInSmallCase = input.toLowerCase();
     //finding out the main word from given input to process further as they have many task related
     //if match found matchFound is an array of result or null otherwise
-    const matchFound = inputInSmallCase.match(
-      /\bshopping\b|\bnewspaper\b|\bclean\b/
-    );
-    const mainWord = matchFound ? matchFound[0] : '';
+    const matchFound = inputInSmallCase.match(/\bshopping\b|\bclean\b/);
+    const mainWord = matchFound ? matchFound[0] : inputInSmallCase;
     switch (mainWord) {
       case 'shopping':
         console.log('match found for shopping');
         break;
-      case 'newspaper':
+      case 'fetch the newspaper.':
+        console.log('match found for newspaper');
         break;
       case 'clean':
+        console.log('match found for clean');
+
+        break;
+      case "how's the weather outside?":
+        console.log(
+          'Q:',
+          input,
+          "\nA.It's pleasant outside. You should take a walk."
+        );
         break;
       default:
         console.log('Q:', input, "\nA.Hmm.. I don't know that");
